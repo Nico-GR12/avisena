bd9f191c917cfc2e8a844a61f1911aafb1e3740d
Nico-GR12 <nicolasgiraldoramirez3024@gmail.com>
2025-09-25 10:47:38 -0500

    comentado rescue

--- app/crud/rescue.py
+++ app/crud/rescue.py
@@ -1,74 +1,74 @@
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import text
-from typing import Optional
-import logging
+# from sqlalchemy.orm import Session
+# from sqlalchemy.exc import SQLAlchemyError
+# from sqlalchemy import text
+# from typing import Optional
+# import logging
 
-from app.schemas.rescue import RescueCreate, RescueUpdate
+# from app.schemas.rescue import RescueCreate, RescueUpdate
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-def create_rescue(db: Session, rescue: RescueCreate) -> Optional[bool]:
-    try:
-        query = text("""
-            INSERT INTO salvamento (
-                id_galpon, fecha, id_tipo_gallina, cantidad_gallinas
-            ) VALUES (
-                :id_galpon, :fecha, :id_tipo_gallina, :cantidad_gallinas
-            )
-        """)
-        db.execute(query, rescue.model_dump())
-        db.commit()
-        return True
-    except SQLAlchemyError as e:
-        db.rollback()
-        logger.error(f"Error al crear salvamento: {e}")
-        raise Exception("Error de base de datos al crear la salvamento")
-
-
-def get_rescue_by_id(db: Session, id_salvamento: int):
-    try:
-        query = text("""SELECT id_galpon, fecha, id_tipo_gallina, cantidad_gallinas, nombre_galpon, nombre_tipo_gallina
-                        FROM salvamento
-                        JOIN galpones ON salvamento.id_galpon = galpones.id_galpon
-                        JOIN tipo_gallinas ON salvamento.id_tipo_gallina = tipo_gallinas.id_tipo_gallina
-                        WHERE id_salvamento = :salvamento_id
-                    """)
-        result = db.execute(query, {"salvamento_id": id_salvamento}).mappings().first()
-        return result
-    except SQLAlchemyError as e:
-        logger.error(f"Error al obtener salvamento por id: {e}")
-        raise Exception("Error de base de datos al obtener la salvamento")
+# def create_rescue(db: Session, rescue: RescueCreate) -> Optional[bool]:
+#     try:
+#         query = text("""
+#             INSERT INTO salvamento (
+#                 id_galpon, fecha, id_tipo_gallina, cantidad_gallinas
+#             ) VALUES (
+#                 :id_galpon, :fecha, :id_tipo_gallina, :cantidad_gallinas
+#             )
+#         """)
+#         db.execute(query, rescue.model_dump())
+#         db.commit()
+#         return True
+#     except SQLAlchemyError as e:
+#         db.rollback()
+#         logger.error(f"Error al crear salvamento: {e}")
+#         raise Exception("Error de base de datos al crear la salvamento")
 
 
-def update_rescue_by_id(db: Session, id_salvamento: int, rescue:RescueUpdate) -> Optional[bool]:
-    try:
-        # Solo los campos enviados por el cliente
-        rescue_data = rescue.model_dump(exclude_unset=True, exclude_none=True)
+# def get_rescue_by_id(db: Session, id_salvamento: int):
+#     try:
+#         query = text("""SELECT id_galpon, fecha, id_tipo_gallina, cantidad_gallinas, nombre_galpon, nombre_tipo_gallina
+#                         FROM salvamento
+#                         JOIN galpones ON salvamento.id_galpon = galpones.id_galpon
+#                         JOIN tipo_gallinas ON salvamento.id_tipo_gallina = tipo_gallinas.id_tipo_gallina
+#                         WHERE id_salvamento = :salvamento_id
+#                     """)
+#         result = db.execute(query, {"salvamento_id": id_salvamento}).mappings().first()
+#         return result
+#     except SQLAlchemyError as e:
+#         logger.error(f"Error al obtener salvamento por id: {e}")
+#         raise Exception("Error de base de datos al obtener la salvamento")
 
-        rescue_data = {key: value for key, value in rescue_data.items() if value != 0}
+
+# def update_rescue_by_id(db: Session, id_salvamento: int, rescue:RescueUpdate) -> Optional[bool]:
+#     try:
+#         # Solo los campos enviados por el cliente
+#         rescue_data = rescue.model_dump(exclude_unset=True, exclude_none=True)
+
+#         rescue_data = {key: value for key, value in rescue_data.items() if value != 0}
         
-        if not rescue_data:
-            return False  # nada que actualizar
+#         if not rescue_data:
+#             return False  # nada que actualizar
         
-        logger.info(f"Actualizando finca {id_salvamento} con datos: {rescue_data}")
+#         logger.info(f"Actualizando finca {id_salvamento} con datos: {rescue_data}")
 
-        # Construir dinámicamente la sentencia UPDATE
-        set_clauses = ", ".join([f"{key} = :{key}" for key in rescue_data.keys()])
-        sentencia = text(f"""
-            UPDATE salvamento 
-            SET {set_clauses}
-            WHERE id_salvamento = :id_salvamento
-        """)
+#         # Construir dinámicamente la sentencia UPDATE
+#         set_clauses = ", ".join([f"{key} = :{key}" for key in rescue_data.keys()])
+#         sentencia = text(f"""
+#             UPDATE salvamento 
+#             SET {set_clauses}
+#             WHERE id_salvamento = :id_salvamento
+#         """)
 
-        # Agregar el id_usuario
-        rescue_data["id_salvamento"] = id_salvamento
+#         # Agregar el id_usuario
+#         rescue_data["id_salvamento"] = id_salvamento
 
-        result = db.execute(sentencia, rescue_data)
-        db.commit()
-        return result.rowcount > 0
-    except SQLAlchemyError as e:
-        db.rollback()
-        logger.error(f"Error al actualizar salvamento {id_salvamento}: {e}")
-        raise Exception("Error de base de datos al actualizar la salvamento")
+#         result = db.execute(sentencia, rescue_data)
+#         db.commit()
+#         return result.rowcount > 0
+#     except SQLAlchemyError as e:
+#         db.rollback()
+#         logger.error(f"Error al actualizar salvamento {id_salvamento}: {e}")
+#         raise Exception("Error de base de datos al actualizar la salvamento")
     
--- app/router/rescue.py
+++ app/router/rescue.py
@@ -1,72 +1,72 @@
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
-from app.crud.permisos import verify_permissions
-from app.router.dependencias import get_current_user
-from app.schemas.rescue import RescueCreate, RescueOut, RescueUpdate
-from core.database import get_db
-from app.schemas.users import UserOut
-from app.crud import rescue as crud_rescue
+# from typing import List
+# from fastapi import APIRouter, Depends, HTTPException, status
+# from sqlalchemy.orm import Session
+# from sqlalchemy.exc import SQLAlchemyError
+# from app.crud.permisos import verify_permissions
+# from app.router.dependencias import get_current_user
+# from app.schemas.rescue import RescueCreate, RescueOut, RescueUpdate
+# from core.database import get_db
+# from app.schemas.users import UserOut
+# from app.crud import rescue as crud_rescue
 
-router = APIRouter()
-modulo = 5
+# router = APIRouter()
+# modulo = 5
 
-@router.post("/crear", status_code=status.HTTP_201_CREATED)
-def create_rescue(
-    rescue: RescueCreate, 
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    try:
-        # El rol de quien usa el endpoint
-        id_rol = user_token.id_rol
+# @router.post("/crear", status_code=status.HTTP_201_CREATED)
+# def create_rescue(
+#     rescue: RescueCreate, 
+#     db: Session = Depends(get_db),
+#     user_token: UserOut = Depends(get_current_user)
+# ):
+#     try:
+#         # El rol de quien usa el endpoint
+#         id_rol = user_token.id_rol
 
 
-        if not verify_permissions(db, id_rol, modulo, 'insertar'):
-            raise HTTPException(status_code=401, detail="Salvamento no autorizado")
+#         if not verify_permissions(db, id_rol, modulo, 'insertar'):
+#             raise HTTPException(status_code=401, detail="Salvamento no autorizado")
 
-        crud_rescue.create_rescue(db, rescue)
-        return {"message": "Salvamento creado correctamente"}
+#         crud_rescue.create_rescue(db, rescue)
+#         return {"message": "Salvamento creado correctamente"}
 
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#     except SQLAlchemyError as e:
+#         raise HTTPException(status_code=500, detail=str(e))
     
 
-@router.get("/by-id/{id_salvamento}", response_model=RescueOut)
-def get_rescue(
-    id_salvamento: int, 
-    db: Session = Depends(get_db), 
-    user_token: UserOut = Depends(get_current_user)
-    ):
+# @router.get("/by-id/{id_salvamento}", response_model=RescueOut)
+# def get_rescue(
+#     id_salvamento: int, 
+#     db: Session = Depends(get_db), 
+#     user_token: UserOut = Depends(get_current_user)
+#     ):
 
-    try:
-        id_rol = user_token.id_rol 
+#     try:
+#         id_rol = user_token.id_rol 
 
-        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
-            raise HTTPException(status_code=401, detail="Salvamento no autorizado")
+#         if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+#             raise HTTPException(status_code=401, detail="Salvamento no autorizado")
         
 
-        rescue = crud_rescue.get_rescue_by_id(db, id_salvamento)
-        if not rescue:
-            raise HTTPException(status_code=404, detail="Finca no encontrada")
-        return rescue
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))    
+#         rescue = crud_rescue.get_rescue_by_id(db, id_salvamento)
+#         if not rescue:
+#             raise HTTPException(status_code=404, detail="Finca no encontrada")
+#         return rescue
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))    
     
 
-@router.put("/by-id/{id_salvamento}")
-def update_user(
-    id_salvamento: int, 
-    rescue: RescueUpdate, 
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    try:
-        success = crud_rescue.update_rescue_by_id(db, id_salvamento, rescue)
-        if not success:
-            raise HTTPException(status_code=400, detail="No se pudo actualizar el salvamento")
-        return {"message": "salvamento actualizado correctamente"}
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @router.put("/by-id/{id_salvamento}")
+# def update_user(
+#     id_salvamento: int, 
+#     rescue: RescueUpdate, 
+#     db: Session = Depends(get_db),
+#     user_token: UserOut = Depends(get_current_user)
+# ):
+#     try:
+#         success = crud_rescue.update_rescue_by_id(db, id_salvamento, rescue)
+#         if not success:
+#             raise HTTPException(status_code=400, detail="No se pudo actualizar el salvamento")
+#         return {"message": "salvamento actualizado correctamente"}
+#     except SQLAlchemyError as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
